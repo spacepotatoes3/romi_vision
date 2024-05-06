8b03d7f23fc68de6bca9b9e0e5de7e7bd9a62f06
Matthew Goff <mgoff5000@gmail.com>
2024-05-05 19:44:52 -0700

    Initial pi vision program

---
+++ src/main/java/Main.java
@@ -0,0 +1,449 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import edu.wpi.first.apriltag.AprilTagDetection;
+import edu.wpi.first.apriltag.AprilTagDetector;
+//import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.cameraserver.CameraServer;
+//import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.MjpegServer;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.cscore.VideoSource;
+import edu.wpi.first.networktables.NetworkTableEvent;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.vision.VisionPipeline;
+import edu.wpi.first.vision.VisionThread;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+/*
+   JSON format:
+   {
+       "team": <team number>,
+       "ntmode": <"client" or "server", "client" if unspecified>
+       "cameras": [
+           {
+               "name": <camera name>
+               "path": <path, e.g. "/dev/video0">
+               "pixel format": <"MJPEG", "YUYV", etc>   // optional
+               "width": <video mode width>              // optional
+               "height": <video mode height>            // optional
+               "fps": <video mode fps>                  // optional
+               "brightness": <percentage brightness>    // optional
+               "white balance": <"auto", "hold", value> // optional
+               "exposure": <"auto", "hold", value>      // optional
+               "properties": [                          // optional
+                   {
+                       "name": <property name>
+                       "value": <property value>
+                   }
+               ],
+               "stream": {                              // optional
+                   "properties": [
+                       {
+                           "name": <stream property name>
+                           "value": <stream property value>
+                       }
+                   ]
+               }
+           }
+       ]
+       "switched cameras": [
+           {
+               "name": <virtual camera name>
+               "key": <network table key used for selection>
+               // if NT value is a string, it's treated as a name
+               // if NT value is a double, it's treated as an integer index
+           }
+       ]
+   }
+ */
+
+public final class Main {
+  private static String configFile = "/boot/frc.json";
+
+  @SuppressWarnings("MemberName")
+  public static class CameraConfig {
+    public String name;
+    public String path;
+    public JsonObject config;
+    public JsonElement streamConfig;
+  }
+
+  @SuppressWarnings("MemberName")
+  public static class SwitchedCameraConfig {
+    public String name;
+    public String key;
+  };
+
+  public static int team;
+  public static boolean server;
+  public static List<CameraConfig> cameraConfigs = new ArrayList<>();
+  public static List<SwitchedCameraConfig> switchedCameraConfigs = new ArrayList<>();
+  public static List<VideoSource> cameras = new ArrayList<>();
+
+  private Main() {
+  }
+
+  /**
+   * Report parse error.
+   */
+  public static void parseError(String str) {
+    System.err.println("config error in '" + configFile + "': " + str);
+  }
+
+  /**
+   * Read single camera configuration.
+   */
+  public static boolean readCameraConfig(JsonObject config) {
+    CameraConfig cam = new CameraConfig();
+
+    // name
+    JsonElement nameElement = config.get("name");
+    if (nameElement == null) {
+      parseError("could not read camera name");
+      return false;
+    }
+    cam.name = nameElement.getAsString();
+
+    // path
+    JsonElement pathElement = config.get("path");
+    if (pathElement == null) {
+      parseError("camera '" + cam.name + "': could not read path");
+      return false;
+    }
+    cam.path = pathElement.getAsString();
+
+    // stream properties
+    cam.streamConfig = config.get("stream");
+
+    cam.config = config;
+
+    cameraConfigs.add(cam);
+    return true;
+  }
+
+  /**
+   * Read single switched camera configuration.
+   */
+  public static boolean readSwitchedCameraConfig(JsonObject config) {
+    SwitchedCameraConfig cam = new SwitchedCameraConfig();
+
+    // name
+    JsonElement nameElement = config.get("name");
+    if (nameElement == null) {
+      parseError("could not read switched camera name");
+      return false;
+    }
+    cam.name = nameElement.getAsString();
+
+    // path
+    JsonElement keyElement = config.get("key");
+    if (keyElement == null) {
+      parseError("switched camera '" + cam.name + "': could not read key");
+      return false;
+    }
+    cam.key = keyElement.getAsString();
+
+    switchedCameraConfigs.add(cam);
+    return true;
+  }
+
+  /**
+   * Read configuration file.
+   */
+  @SuppressWarnings("PMD.CyclomaticComplexity")
+  public static boolean readConfig() {
+    // parse file
+    JsonElement top;
+    try {
+      top = new JsonParser().parse(Files.newBufferedReader(Paths.get(configFile)));
+    } catch (IOException ex) {
+      System.err.println("could not open '" + configFile + "': " + ex);
+      return false;
+    }
+
+    // top level must be an object
+    if (!top.isJsonObject()) {
+      parseError("must be JSON object");
+      return false;
+    }
+    JsonObject obj = top.getAsJsonObject();
+
+    // team number
+    JsonElement teamElement = obj.get("team");
+    if (teamElement == null) {
+      parseError("could not read team number");
+      return false;
+    }
+    team = teamElement.getAsInt();
+
+    // ntmode (optional)
+    if (obj.has("ntmode")) {
+      String str = obj.get("ntmode").getAsString();
+      if ("client".equalsIgnoreCase(str)) {
+        server = false;
+      } else if ("server".equalsIgnoreCase(str)) {
+        server = true;
+      } else {
+        parseError("could not understand ntmode value '" + str + "'");
+      }
+    }
+
+    // cameras
+    JsonElement camerasElement = obj.get("cameras");
+    if (camerasElement == null) {
+      parseError("could not read cameras");
+      return false;
+    }
+    JsonArray cameras = camerasElement.getAsJsonArray();
+    for (JsonElement camera : cameras) {
+      if (!readCameraConfig(camera.getAsJsonObject())) {
+        return false;
+      }
+    }
+
+    if (obj.has("switched cameras")) {
+      JsonArray switchedCameras = obj.get("switched cameras").getAsJsonArray();
+      for (JsonElement camera : switchedCameras) {
+        if (!readSwitchedCameraConfig(camera.getAsJsonObject())) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * Start running the camera.
+   */
+  public static VideoSource startCamera(CameraConfig config) {
+    System.out.println("Starting camera '" + config.name + "' on " + config.path);
+    UsbCamera camera = new UsbCamera(config.name, config.path);
+    MjpegServer server = CameraServer.startAutomaticCapture(camera);
+
+    Gson gson = new GsonBuilder().create();
+
+    camera.setConfigJson(gson.toJson(config.config));
+    camera.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen);
+
+    if (config.streamConfig != null) {
+      server.setConfigJson(gson.toJson(config.streamConfig));
+    }
+
+    return camera;
+  }
+
+  /**
+   * Start running the switched camera.
+   */
+  public static MjpegServer startSwitchedCamera(SwitchedCameraConfig config) {
+    System.out.println("Starting switched camera '" + config.name + "' on " + config.key);
+    MjpegServer server = CameraServer.addSwitchedCamera(config.name);
+
+    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    inst.addListener(
+        inst.getTopic(config.key),
+        EnumSet.of(NetworkTableEvent.Kind.kImmediate, NetworkTableEvent.Kind.kValueAll),
+        event -> {
+          if (event.valueData != null) {
+            if (event.valueData.value.isInteger()) {
+              int i = (int) event.valueData.value.getInteger();
+              if (i >= 0 && i < cameras.size()) {
+                server.setSource(cameras.get(i));
+              }
+            } else if (event.valueData.value.isDouble()) {
+              int i = (int) event.valueData.value.getDouble();
+              if (i >= 0 && i < cameras.size()) {
+                server.setSource(cameras.get(i));
+              }
+            } else if (event.valueData.value.isString()) {
+              String str = event.valueData.value.getString();
+              for (int i = 0; i < cameraConfigs.size(); i++) {
+                if (str.equals(cameraConfigs.get(i).name)) {
+                  server.setSource(cameras.get(i));
+                  break;
+                }
+              }
+            }
+          }
+        });
+
+    return server;
+  }
+
+  /**
+   * Example pipeline.
+   */
+  public static class MyPipeline implements VisionPipeline {
+    public int val;
+    public Mat input_mat;
+    public Mat april_mat;
+    public Mat proc_mat;
+    public AprilTagDetector detector = initializeDetector();
+    public ArrayList<Long> tags = new ArrayList<>();
+    public Scalar outlineColor = new Scalar(0, 255, 0);
+    public Scalar crossColor = new Scalar(0, 0, 255);
+
+    private static AprilTagDetector initializeDetector() {
+      AprilTagDetector myDetector = new AprilTagDetector();
+      // look for tag36h11, correct 1 error bits
+      myDetector.addFamily("tag36h11", 1);
+      return myDetector;
+    }
+
+    @Override
+    public void process(Mat mat) {
+      val += 1;
+      if (input_mat == null) {
+        input_mat = mat.clone();
+      }
+       if (april_mat == null) {
+        april_mat = mat.clone();
+      }
+      mat.copyTo(input_mat);
+
+      //find april tags in the image
+      Imgproc.cvtColor(mat, april_mat, Imgproc.COLOR_RGB2GRAY);
+
+      AprilTagDetection[] detections = detector.detect(april_mat);
+
+      // have not seen any tags yet
+      tags.clear();
+
+      for (AprilTagDetection detection : detections) {
+        // remember we saw this tag
+        tags.add((long) detection.getId());
+
+        // draw lines around the tag
+        for (var i = 0; i <= 3; i++) {
+          var j = (i + 1) % 4;
+          var pt1 = new Point(detection.getCornerX(i), detection.getCornerY(i));
+          var pt2 = new Point(detection.getCornerX(j), detection.getCornerY(j));
+          Imgproc.line(april_mat, pt1, pt2, outlineColor, 2);
+        }
+
+        // mark the center of the tag
+        var cx = detection.getCenterX();
+        var cy = detection.getCenterY();
+        var ll = 10;
+        Imgproc.line(april_mat, new Point(cx - ll, cy), new Point(cx + ll, cy), crossColor, 2);
+        Imgproc.line(april_mat, new Point(cx, cy - ll), new Point(cx, cy + ll), crossColor, 2);
+
+        // identify the tag
+        Imgproc.putText(
+            april_mat,
+            Integer.toString(detection.getId()),
+            new Point(cx + ll, cy),
+            Imgproc.FONT_HERSHEY_SIMPLEX,
+            1,
+            crossColor,
+            3);
+      }
+
+
+
+      // Put a rectangle on the image as example vision flow
+      Imgproc.rectangle(
+          mat, new Point(100, 100), new Point(400, 400), new Scalar(255, 255, 255), 5);
+      // put modified image on public instance var for process to read and put out
+      // outputstream
+      proc_mat = mat;
+    }
+
+  }
+
+  /**
+   * Main.
+   */
+  public static void main(String... args) {
+    if (args.length > 0) {
+      configFile = args[0];
+    }
+
+    // read configuration
+    if (!readConfig()) {
+      return;
+    }
+
+    // start NetworkTables
+    NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
+    if (server) {
+      System.out.println("Setting up NetworkTables server");
+      ntinst.startServer();
+    } else {
+      System.out.println("Setting up NetworkTables client for team " + team);
+      ntinst.startClient4("wpilibpi");
+      ntinst.setServerTeam(team);
+      ntinst.startDSClient();
+    }
+
+    // start cameras
+    for (CameraConfig config : cameraConfigs) {
+      cameras.add(startCamera(config));
+    }
+
+    // start switched cameras
+    for (SwitchedCameraConfig config : switchedCameraConfigs) {
+      startSwitchedCamera(config);
+    }
+
+    // Get a CvSink. This will capture Mats from the camera
+    // CvSink cvSink = CameraServer.getVideo();
+    // Setup a CvSource. This will send images back to the Dashboard
+    CvSource rawOutputStream = CameraServer.putVideo("Feed", 640, 480);
+    CvSource outputStream = CameraServer.putVideo("Rectangle", 640, 480);
+    CvSource aprilStream = CameraServer.putVideo("Aprilcodes", 640, 480);
+
+    // start image processing on camera 0 if present
+    if (cameras.size() >= 1) {
+      VisionThread visionThread = new VisionThread(cameras.get(0),
+          new MyPipeline(), pipeline -> {
+            // do something with pipeline results
+            // System.out.println("pipeline");
+            ntinst.getTable("Vision").getEntry("TEST").setNumber(pipeline.val);
+            rawOutputStream.putFrame(pipeline.input_mat);
+            outputStream.putFrame(pipeline.proc_mat);
+            aprilStream.putFrame(pipeline.april_mat);
+          });
+      /*
+       * something like this for GRIP:
+       * VisionThread visionThread = new VisionThread(cameras.get(0),
+       * new GripPipeline(), pipeline -> {
+       * ...
+       * });
+       */
+      visionThread.start();
+    }
+
+    // loop forever
+    for (;;) {
+      try {
+        Thread.sleep(10000);
+      } catch (InterruptedException ex) {
+        return;
+      }
+    }
+  }
+}
